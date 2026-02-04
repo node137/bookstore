ab65cb83aaf9ca72466b9f21e6003d962a9e275a
noora <noorah85@gmail.com>
2026-02-04 23:59:23 +0200

    lisätty kirjan lisääminen

--- bookstore/src/main/java/backend/bookstore/web/BookController.java
+++ bookstore/src/main/java/backend/bookstore/web/BookController.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import backend.bookstore.model.Book;
 import backend.bookstore.model.BookRepository;
 
 @Controller
@@ -27,6 +29,18 @@ public class BookController {
     public String showBooks(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "/books";
+    }
+
+    @GetMapping("/addBook")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook"; // addbook.html
+    }
+
+    @PostMapping("/saveBook")
+    public String saveBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/books";
     }
 
     @GetMapping("/deleteBook/{id}")
