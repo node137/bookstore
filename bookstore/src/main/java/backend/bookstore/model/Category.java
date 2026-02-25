66961523c82a1dd034df38e417344487dc7b49b4
noora <noorah85@gmail.com>
2026-02-25 20:07:19 +0200

    restcontrollerit

--- bookstore/src/main/java/backend/bookstore/model/Category.java
+++ bookstore/src/main/java/backend/bookstore/model/Category.java
@@ -2,6 +2,8 @@ package backend.bookstore.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +18,7 @@ public class Category {
 
     private String name;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "category")
     private List<Book> books;
 
---
+++ bookstore/src/main/java/backend/bookstore/web/BookRestController.java
@@ -0,0 +1,34 @@
+package backend.bookstore.web;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import backend.bookstore.model.Book;
+import backend.bookstore.model.BookRepository;
+
+@RestController
+public class BookRestController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    // Return all books as JSON
+    @GetMapping("/api/books")
+    public List<Book> getBooks() {
+        List<Book> books = new ArrayList<>();
+        bookRepository.findAll().forEach(books::add);
+        return books;
+    }
+
+    // Return one book by id as JSON
+    @GetMapping("/api/books/{id}")
+    public Optional<Book> getBookById(@PathVariable("id") Long id) {
+        return bookRepository.findById(id);
+    }
+}
