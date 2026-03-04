0db7168d8e0f7e571dcda1d6c76aa852ec713c5f
noora <noorah85@gmail.com>
2026-03-04 22:19:40 +0200

    korjattu kääntymisen ongelmat, kaksi samaa polkua eri controllereissa

--- bookstore/src/main/java/backend/bookstore/web/BookRestController.java
+++ bookstore/src/main/java/backend/bookstore/web/BookRestController.java
@@ -7,15 +7,16 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import backend.bookstore.model.Book;
 import backend.bookstore.model.BookRepository;
 
 @RestController
+@RequestMapping("/api")
 public class BookRestController {
 
-    @Autowired
     private BookRepository bookRepository;
 
     // Return all books as JSON
